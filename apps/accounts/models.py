42e4194bcaed7f6182c31900df5fe1d6b2f33066
Surya <cssurya2006@gmail.com>
2026-05-30 11:39:29 +0530

    fixed users

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -16,7 +16,7 @@ class CustomUser(AbstractUser):
     role = models.CharField(
         max_length=20,
         choices=Role.choices,
-        default=Role.AUTHOR,
+        default=Role.CLIENT,
         verbose_name='User Role',
     )
     avatar = models.ImageField(
